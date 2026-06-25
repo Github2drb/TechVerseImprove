0231b7f262529508dff53ccc55a5ae758453df97
D Ramesh Babu <77008896+Github2drb@users.noreply.github.com>
2026-06-25 12:46:44 +0530

    Change cssMinify option to use esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    cssMinify: 'false', 
+    cssMinify: 'esbuild', 
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
