dbbff7e829b41a625088082bd2d7b7d74aa04fe3
D Ramesh Babu <77008896+Github2drb@users.noreply.github.com>
2026-09-17 15:48:26 +0530

    Delete server/models/User.ts

--- server/models/User.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  role: String,
-});
-
-export default mongoose.model("User", UserSchema);
